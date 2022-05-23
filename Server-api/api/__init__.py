62d05d46f7eeb5a4a066a06cc03643b7d51fd25c
isanghyeon <cpd4268@naver.com>
2022-05-23 16:46:47 +0900

    Updated; 2022.05.23

---
+++ Server-api/api/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Python Programming Project - Team 3
+    ~~~~~~~~~~~~~~
+    A brief description goes here.
+    :copyright: (c) 2022 by isanghyeon.
+
+    The MIT License
+    Copyright (c) 2022 isanghyeon all rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+"""
+
+from flask import Blueprint
+from flask_restx import Api
+
+NAME = 'api'
+bp = Blueprint(
+    NAME,
+    __name__,
+    url_prefix='/'
+)
+
+api = Api(
+    bp,
+    version='2022.05.23',
+    title='TEAM-3 API',
+    description='Python Programming Messenger Service Develop'
+)
+
+api.add_namespace()
