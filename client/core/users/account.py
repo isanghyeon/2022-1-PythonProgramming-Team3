a0542e897538b68711ea4b5dce7c8a3af0c20f45
isanghyeon <cpd4268@naver.com>
2022-06-03 23:16:36 +0900

    update

--- client/core/users/account.py
+++ client/core/users/account.py
@@ -28,33 +28,30 @@
     THE SOFTWARE.
 """
 
-import socket
-import threading
+from feature.api import User as UserAPI
 
 
-def Send(client_sock):
-    while True:
-        send_data = bytes(
-            input().encode()
-        )
-        client_sock.send(send_data)
+class AccountValidator:
+    def __init__(self):
+        pass
 
 
-def Recv(client_sock):
-    while True:
-        recv_data = client_sock.recv(1024).decode()
-        print(recv_data)
+class Login:
+    def __init__(self, UserName, UserPassword):
+        self.UserName = UserName
+        self.UserPassword = UserPassword
+        self.ResultObject = None
+
+    def getUserData(self):
+        self.ResultObject = UserAPI.Get(ActType=False, data={"key": None, "UserName": f"{self.UserName}", "UserAccountPW": f"{self.UserPassword}"})
+
+        if self.ResultObject["status"] == 200:
+            return self.ResultObject["status"], self.ResultObject["message"]
+
+        return self.ResultObject["status"], self.ResultObject["message"]
 
 
-if __name__ == '__main__':
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Host = '0.0.0.0'
-    Port = 30000
-    client_sock.connect((Host, Port))
-    print(f"Connected to {Host}:{Port}")
 
-    thread1 = threading.Thread(target=Send, args=(client_sock,))
-    thread1.start()
-
-    thread2 = threading.Thread(target=Recv, args=(client_sock,))
-    thread2.start()
+class Register:
+    def __init__(self):
+        pass
